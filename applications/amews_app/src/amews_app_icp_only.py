56a00776199682526bc4a6015b62aa371a9de7cd
Ginsburg <tginsburg@anl.gov>
2025-07-18 19:07:35 -0500

    adding icp only

--- applications/amews_app/src/amews_app_icp_only.py
+++ applications/amews_app/src/amews_app_icp_only.py
@@ -7,6 +7,7 @@ from madsci.common.types.step_types import StepDefinition
 from madsci.client.experiment_application import ExperimentApplication, ExperimentDesign
 from madsci.client.node.rest_node_client import RestNodeClient
 from pathlib import Path
+from utils.log_parsing import read_logs, add_timestamps
 from utils.generate_protocol import generate_protocol
 from utils.assign_timestamps import assign_timestamps
 from utils.create_sample_file import create_sample_file
@@ -27,7 +28,9 @@ class AMEWSApp(ExperimentApplication):
     
 
 if __name__ == "__main__":
+    
     experiment_app = AMEWSApp()
+    
     current_time = datetime.datetime.now()
     with experiment_app.manage_experiment(
         run_name=f"AMEWS Experiment Run {current_time}",
@@ -47,6 +50,7 @@ if __name__ == "__main__":
         sampling_delay_mins = 180
         input_variables_path = experiment_app.network_input_path / "input_variables.json"
         labjack_client = RestNodeClient("http://146.139.45.9:2001")
+
 
         with open(input_variables_path, "r") as f:
             input_variables = json.load(f)
@@ -85,49 +89,14 @@ if __name__ == "__main__":
         bk_workflow = None
         icp_workflow = None
         first_run = True
+        icp_racks = 3
+        log_file_path = Path(".").resolve()
+        log_files = ["log1.log", "log2.log", "log3.log"]
+        
         
         input_locations = ["supply_slot_1", "supply_slot_2", "supply_slot_3", "supply_slot_4", "supply_slot_5"]
-        while len(measured_racks) < total_racks:
-            bk_workflow = experiment_app.workcell_client.query_workflow(bk_workflow.workflow_id) if bk_workflow else None
-            icp_workflow = experiment_app.workcell_client.query_workflow(icp_workflow.workflow_id) if icp_workflow else None
-            
-            if bk_workflow and bk_workflow.status.completed:
-                
-                experiment_app.workcell_client.submit_workflow(experiment_app.workflow_directory / "open_door.workflow.yaml")
-                experiment_app.workcell_client.submit_workflow(
-                   experiment_app.workflow_directory / "transfer_from_bk.workflow.yaml", parameters={"target_location": input_locations[len(sampled_racks)]}
-                )
-                experiment_app.workcell_client.submit_workflow(experiment_app.workflow_directory / "close_door.workflow.yaml")
-                protocol_file_path = experiment_app.output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}_stamped.json"
-                completed_protocol = experiment_app.data_client.save_datapoint_value(
-                bk_workflow.get_datapoint_id_by_label("protocol"), protocol_file_path
-                )
-                try: 
-                    protocol_file_path = experiment_app.network_output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}_stamped.json"
-                    completed_protocol = experiment_app.data_client.save_datapoint_value(
-                    bk_workflow.get_datapoint_id_by_label("protocol"), protocol_file_path
-                    )
-                except Exception:
-                    print("unable to write to network drive")
-                    
-                with open(protocol_file_path, "r") as f:
-                    timestamped_protocol = BigKahunaProtocol(**json.load(f))
-
-                sampled_racks.append(assign_timestamps(timestamped_protocol, latest_input_rack))
-
-                with open(experiment_app.output_path / f"tube_rack_{len(sampled_racks)}_info.json", "w") as f:
-                    json.dump({key: value.model_dump() for key, value in sampled_racks[-1].items()}, f, indent=4)
-                try:
-                    with open(experiment_app.network_output_path / f"tube_rack_{len(sampled_racks)}_info.json", "w") as f:
-                        json.dump({key: value.model_dump() for key, value in sampled_racks[-1].items()}, f, indent=4)
-                except Exception as e:
-                    print("unable to write to network")
-                bk_workflow = None
-            
-        
-            if bk_workflow is None and len(sampled_racks) < total_racks:
-                
-                protocol, latest_input_rack, starting_cell_index, current_samples = generate_protocol(first_run=first_run, 
+        for i in range(icp_racks):
+            protocol, latest_input_rack, starting_cell_index, current_samples = generate_protocol(first_run=first_run, 
                                                                                         num_cells=num_cells, 
                                                                                         input_chemicals=input_chemicals,
                                                                                         cell_volumes=input_volumes, 
@@ -137,26 +106,42 @@ if __name__ == "__main__":
                                                                                             aliquots=aliquots,
                                                                                             sampling_delay = sampling_delay_mins
                                                                                         )
-                protocol_path = experiment_app.output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}.json"
-                with open(protocol_path, "w") as f:
+            protocol_path = experiment_app.output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}.json"
+            with open(protocol_path, "w") as f:
+                json.dump(protocol.model_dump(), f)
+            try:
+                network_protocol_path = experiment_app.network_output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}.json"
+                with open(network_protocol_path, "w") as f:
                     json.dump(protocol.model_dump(), f)
-                try:
-                     network_protocol_path = experiment_app.network_output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}.json"
-                     with open(network_protocol_path, "w") as f:
-                        json.dump(protocol.model_dump(), f)
-                except Exception as e:
-                    print("unable to write protocol to network")
-                first_run = False
-                experiment_app.workcell_client.submit_workflow(experiment_app.workflow_directory / "open_door.workflow.yaml")
-                experiment_app.workcell_client.submit_workflow(
-                    experiment_app.workflow_directory / "transfer_to_bk.workflow.yaml", parameters={"source_location": input_locations[len(sampled_racks)]}
-                )
-                experiment_app.workcell_client.submit_workflow(experiment_app.workflow_directory / "close_door.workflow.yaml")
-                bk_workflow = experiment_app.workcell_client.submit_workflow(
-                        experiment_app.workflow_directory / "run_bk.workflow.yaml", parameters={"protocol": str(protocol_path)},
-                        await_completion = False
-                    )
-                
+            except Exception as e:
+                print("unable to write protocol to network")
+            first_run = False
+            steps = read_logs(str(log_file_path / log_files[i]))
+            protocol = add_timestamps(steps, protocol)
+            protocol_path = experiment_app.output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}_stamped.json"
+            
+            with open(protocol_path, "w") as f:
+                json.dump(protocol.model_dump(), f)
+            try:
+                network_protocol_path = experiment_app.network_output_path / "protocols" / f"tube_rack_{len(sampled_racks) + 1}_stamped.json"
+                with open(network_protocol_path, "w") as f:
+                    json.dump(protocol.model_dump(), f)
+            except Exception as e:
+                print("unable to write protocol to network")
+            input_rack = assign_timestamps(protocol, latest_input_rack)
+            sampled_racks.append(input_rack)
+            with open(experiment_app.output_path / f"tube_rack_{len(sampled_racks)}_info.json", "w") as f:
+                    json.dump({key: value.model_dump() for key, value in sampled_racks[-1].items()}, f, indent=4)
+            try:
+                with open(experiment_app.network_output_path / f"tube_rack_{len(sampled_racks)}_info.json", "w") as f:
+                    json.dump({key: value.model_dump() for key, value in sampled_racks[-1].items()}, f, indent=4)
+            except Exception as e:
+                    print("unable to write to network")
+
+        while len(measured_racks) < total_racks:
+            icp_workflow = experiment_app.workcell_client.query_workflow(icp_workflow.workflow_id) if icp_workflow else None
+            
+            
             if icp_workflow is None and len(sampled_racks) > len(measured_racks):
                     labjack_state = labjack_client.get_state()
                     if labjack_state["volumes"]["AIN0"] > 0.95*113.56:
@@ -168,6 +153,13 @@ if __name__ == "__main__":
                         experiment_app.workcell_client.submit_workflow(
                             experiment_app.workflow_directory / "transfer_to_icp.workflow.yaml", parameters={"source_location": input_locations[len(measured_racks)]}
                         )
+                        icp_workflow = WorkflowDefinition.from_yaml(experiment_app.workflow_directory / "run_icp.workflow.yaml")
+                        if len(measured_racks) == 0:
+                            icp_workflow.steps = icp_workflow.steps[0:2]
+                        elif len(measured_racks) == icp_racks - 1:
+                            icp_workflow.steps = icp_workflow.steps[1:]
+                        else:
+                             icp_workflow.steps = [icp_workflow.steps[1]]
                         icp_workflow = experiment_app.workcell_client.submit_workflow(
                         experiment_app.workflow_directory / "run_icp.workflow.yaml", parameters={"dataset_name": dataset_name, "sample_info_file": str(sample_info_file_path)}, await_completion=False)
 
